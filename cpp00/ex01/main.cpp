757ae84ea599291b285cf4a3375cb278f026be7d
Joao Francisco Perpectuo Figueiredo <jperpect@c2r6s2.42porto.com>
2025-02-25 17:43:24 +0000

    the wilee start

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -1,13 +1,24 @@
 
 #include "Contact.hpp"
 #include "PhoneBook.hpp"
+
 int main(void)
 {
 	PhoneBook ok;
-	std::string ola;
+	std::string line;
 	ok.set_new("ola mundo");
 	ok.print_contact(1);
 	Contact olas;
 	olas.set_new_contact();
 	olas.print_contact_line(1);
+
+///	std::cin >> line;
+	
+	while (line != "EXIT")
+	{
+		std::cin.clear();
+		std::cin >> line;
+		std::cout << line<< std::endl;
+	}
+	
 }
