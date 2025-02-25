fd15757cfc7ae612c52df36883a0ecf91412d118
Joao Francisco Perpectuo Figueiredo <jperpect@c2r6s2.42porto.com>
2025-02-25 14:11:01 +0000

    add the cin the comand the input and rule s in makefile

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -3,7 +3,11 @@
 int main(void)
 {
 	PhoneBook ok;
+	std::string ola;
 	ok.set_new("ola mundo");
 	ok.print_contact(1);
-	std::cout << "ok\n";
+	std::cout << "enter the ADD more contact or SEARCH or EXIT" << std::endl;
+	std::cin >> ola;
+	if(ola == (std::string)"ADD")
+		std::cout << "add foi compleot";
 }
