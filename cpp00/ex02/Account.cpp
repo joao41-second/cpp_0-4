b1f7079e50fbd0fea0c922fc55099daae79cb51e
Joao Francisco Perpectuo Figueiredo <jperpect@c2r6s2.42porto.com>
2025-02-26 14:00:40 +0000

    the print

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -4,6 +4,11 @@
 #include <iostream>
 #include <iomanip>
 
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+int Account::_nbAccounts = 0;
+
 void pirnt_time()
 {
 	time_t tm = std::time(NULL);
@@ -20,13 +25,64 @@ void pirnt_time()
 	std::cout << time_data.tm_min;
 	std::cout << std::setfill('0') << std::setw(2);
 	std::cout << time_data.tm_sec;
-
-
-	std::cout << "]"<< std::endl;
-
+	std::cout << "] " ;
 }
 
-int main()
+int Account::getNbAccounts()
+{
+	return (_nbAccounts);
+}
+
+int Account::getNbDeposits()
+{
+	return (_totalNbDeposits);
+}
+
+int Account::getTotalAmount()
+{
+	return (_totalAmount);
+}
+
+int Account::getNbWithdrawals()
+{
+	return (_totalNbWithdrawals);
+}
+
+
+void Account::displayAccountsInfos()
 {
 	pirnt_time();
+	std::cout << "accounts:" << getNbAccounts();
+	std::cout << ";total:" << getTotalAmount();
+	std::cout << ";deposits:" << getNbDeposits();
+	std::cout << "withdrawals:" << getNbWithdrawals() << std::endl;
 }
+
+void Account::displayStatus()const
+{
+
+}
+int Account::checkAmount()const
+{
+return 1;
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	return 1;
+}
+
+void	Account::makeDeposit( int deposit )
+{
+
+}
+
+Account::Account(int)
+{
+
+}
+
+Account::~Account()
+{
+}
+
