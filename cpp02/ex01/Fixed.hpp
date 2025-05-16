23e9af38a77ab4ca80c32ce808a23838679b647a
windo <jfigueiredo095@gmail.com>
2025-05-16 18:02:04 +0100

    new

---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jperpct <jperpect@student.42porto.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/16 15:52:37 by jperpct           #+#    #+#             */
+/*   Updated: 2025/05/16 16:37:37 by jperpct          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <iostream>
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+class Fixed
+{
+	private :
+		int _vaule;
+		static int _fract;
+	public:
+
+	// construc defult
+	 Fixed();
+
+	 // distucter
+	 ~Fixed();
+
+	 // copy construc
+	 Fixed(const Fixed &other);
+	
+	 // atribut operator
+
+	 Fixed &operator=(const Fixed &src);
+
+	 Fixed(const int new_vaule);
+
+	 Fixed(const float new_vaule);
+
+	 int getRawBits() const;
+
+	 void setRawBits(int new_vaule);
+
+	 int toInt(void) const;
+
+	 float toFloat(void) const;
+
+};
+
+
+std::ostream &operator<<(std::ostream &o, Fixed const &fixed);
+
+
+
+
+
+#endif
