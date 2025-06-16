ac7aa7991df5c0d80eef0524e2c5b4d8b31948a2
Joao Francisco Perpectuo Figueiredo <jperpect@c2r6s2.42porto.com>
2025-06-16 11:04:47 +0100

    updtate makefile

--- cpp04/ex00/WorngAnimal.hpp
+++ cpp04/ex00/WorngAnimal.hpp
@@ -6,7 +6,7 @@
 /*   By: jperpct <jperpect@student.42porto.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 10:38:56 by jperpct           #+#    #+#             */
-/*   Updated: 2025/06/16 11:01:19 by jperpct          ###   ########.fr       */
+/*   Updated: 2025/06/16 11:04:26 by jperpct          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ class WorngAnimal
 		WorngAnimal(std::string string);
 		WorngAnimal();
 		WorngAnimal(WorngAnimal &copy);
-	 	~WorngAnimal();
+		virtual ~WorngAnimal();
 		virtual WorngAnimal & operator=(WorngAnimal &var);
 		virtual std::string get_Type() const;
 		void makeSound() const;
